ced06c3f4cf33610f3d7fe097b0077a54bb6723b
Allie <alliecaton3@gmail.com>
2021-01-18 18:22:53 -0500

    commit tree a40bc71d1b182a1e7bae45d31c53c190cd593e61

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name 
+    "#{self.first_name.to_s} #{self.last_name.to_s}"
+  end
+
+  def list_roles
+    self.characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end 
+
 end
+
